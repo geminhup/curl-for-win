a5c1e59de47d372d3c3356b6f6aaac960e280e6e
Viktor Szakats <vszakats@users.noreply.github.com>
2018-03-10 15:12:58 +0000

    fixes

--- _build.sh
+++ _build.sh
@@ -7,7 +7,7 @@
 #   Windows:
 #     MSYS2: zip p7zip mingw-w64-{i686,x86_64}-{clang,jq,osslsigncode} gpg python
 #   Linux
-#     xz p7zip-full binutils-mingw-w64 gcc-mingw-w64 gnupg-curl jq osslsigncode dos2unix realpath wine
+#     xz zip p7zip-full binutils-mingw-w64 gcc-mingw-w64 gnupg-curl jq osslsigncode dos2unix realpath wine
 #   Mac:
 #     brew install xz p7zip mingw-w64 jq osslsigncode dos2unix gpg gnu-sed wine
 
